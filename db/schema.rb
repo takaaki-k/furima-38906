89a02ca8448f7660f5f5bccc8cd5f8323f88530b
takaaki-k <121436318+takaaki-k@users.noreply.github.com>
2023-06-08 22:44:29 +0900

    データベース設定修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_01_131326) do
+ActiveRecord::Schema.define(version: 2023_05_28_062143) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
