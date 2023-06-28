2415b54d4a357b4d9f2092e96a69700f764a37ba
takaaki-k <121436318+takaaki-k@users.noreply.github.com>
2023-06-28 21:52:24 +0900

    トークン化処理

---
+++ app/javascript/card.js
@@ -0,0 +1,25 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_13181f11477f9ba3a9ed4bc2')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const submit = document.getElementById("button");
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+window.addEventListener("load", pay);
