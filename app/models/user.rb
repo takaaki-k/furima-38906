f3cbfd22906d2e4e3c56681d2f9026241b7729bf
takaaki-k <121436318+takaaki-k@users.noreply.github.com>
2023-07-18 21:11:52 +0900

    微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,19 @@ class User < ApplicationRecord
   validates :nickname,
     presence: true
   validates :password,
-    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: " is invalid. Include both letters and numbers" }
   validates :last_name,
     presence: true,
-    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " is invalid. Input full-width characters" }
   validates :first_name,
     presence: true,
-    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " is invalid. Input full-width characters" }
   validates :last_name_kana,
     presence: true,
-    format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+    format: { with: /\A[ァ-ヶー－]+\z/, message: " is invalid. Input full-width katakana characters" }
   validates :first_name_kana,
     presence: true,
-    format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+    format: { with: /\A[ァ-ヶー－]+\z/, message: " is invalid. Input full-width katakana characters" }
   validates :birthday,
     presence: true
 end
